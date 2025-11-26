5ecac3fe080e29234d40a898a5d584a87d190dfa
kr9sis <mkmw464l@anonaddy.com>
2025-11-26 00:54:22 +0000

    Fixed autostart with setup home and replaced Discord with Vesktop

--- hyprland/.config/hypr/setup_home.sh
+++ hyprland/.config/hypr/setup_home.sh
@@ -1,15 +1,16 @@
 #! /bin/env bash
 
-while ! (hyprctl clients | grep "initialClass: brave-browser") && (hyprctl clients | grep "initialClass: kitty") && (hyprctl clients | grep "initialClass: discord") && (hyprctl clients | grep "initialClass: Spotify"); do
+while ! \
+  (hyprctl clients | grep -q "initialClass: brave-browser") && \
+  (hyprctl clients | grep -q "initialClass: vesktop"); do
   sleep 0.2
 done
 
 sleep 5
 
+hyprctl dispatch workspace 1
+
 if hyprctl monitors | grep "description: ASR PG34WQ15R2B"; then
   hyprctl dispatch moveworkspacetomonitor "2 eDP-1"
-  hyprctl dispatch moveworkspacetomonitor "1 DP-1" # Lower USB C port on PC
-  hyprctl dispatch moveworkspacetomonitor "1 DP-3" # Upper USB C port on PC
+  hyprctl dispatch movecurrentworkspacetomonitor +1 # Move workspace 1 to second monitor
 fi
-
-hyprctl dispatch workspace 1
