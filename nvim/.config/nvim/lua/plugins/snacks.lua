d3c86e45e05b48b4d237a78c372b65d5cfa9fa9d
kr9sis <mkmw464l@anonaddy.com>
2025-11-30 12:13:25 +0000

    Made sure explorer also showed hidden files

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,18 +1,7 @@
 return {
     "folke/snacks.nvim",
-    -- opts = {
-    --     scroll = { enabled = false }, -- Disable scrolling animations
-    --     -- picker = { sources = { files = { hidden = true } } },
-    --     -- explorer = { hidden = true },
-    --     explorer = {
-    --         sources = {
-    --             files = {
-    --                 hidden = true,
-    --             },
-    --         },
-    --     },
-    -- },
     opts = {
+        scroll = { enabled = false }, -- Disable scrolling animations
         picker = {
             sources = {
                 files = { hidden = true },
