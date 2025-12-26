909514683a9ccaf665d961ffd2c77b48c4308407
kr9sis <mkmw464l@anonaddy.com>
2025-12-26 11:59:06 +0100

    Switched from pyright to basedpyright for python type checking

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -220,7 +220,7 @@ return {
             local servers = {
                 -- clangd = {},
                 gopls = {},
-                pyright = {},
+                basedpyright = {},
                 bashls = {},
                 lua_ls = {
                     -- cmd = { ... },
