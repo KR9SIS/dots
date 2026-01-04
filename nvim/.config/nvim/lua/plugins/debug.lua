5531cc0235f5a35618e1be24685a997c6666c2bf
kr9sis <mkmw464l@anonaddy.com>
2026-01-04 15:27:44 +0100

    Set up go debugger

--- nvim/.config/nvim/lua/plugins/debug.lua
+++ nvim/.config/nvim/lua/plugins/debug.lua
@@ -123,9 +123,7 @@ return {
         -- dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
         -- Install golang specific config
-        require("dap-go").setup({
-            outputMode = "remote",
-        })
+        require("dap-go").setup({})
         require("dap-python").setup("/home/kr9sis/.virtualenv/debugpy/bin/python")
     end,
 
