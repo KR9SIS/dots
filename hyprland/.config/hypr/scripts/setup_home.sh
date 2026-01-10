ffc782393aff9988036fe6092a19a1765b610510
kr9sis <mkmw464l@anonaddy.com>
2026-01-10 09:56:49 +0100

    Fixed bug where browser opened in workspace 3 by refactoring setup_home.sh

--- hyprland/.config/hypr/scripts/setup_home.sh
+++ hyprland/.config/hypr/scripts/setup_home.sh
@@ -1,12 +1,20 @@
 #! /bin/env bash
 
-while ! \
-  (hyprctl clients | grep -q "initialClass: brave-browser") && \
-  (hyprctl clients | grep -q "initialClass: vesktop"); do
-  sleep 0.2
-done
+setup_app() {
+  local workspace=$1
+  local class=$2
+  local sleep_len=$3
 
-sleep 10
+  while ! (hyprctl clients | grep -q "class: $class"); do
+    sleep 0.2 # simple and inefficient busy waiting loop
+  done
+  sleep "$sleep_len"
+  hyprctl dispatch movetoworkspacesilent "$workspace", "class:$class"
+}
+
+setup_app 1 "brave-browser" 1
+setup_app 2 "kitty" 1
+setup_app 3 "vesktop" 5
 
 hyprctl dispatch workspace 1
 
