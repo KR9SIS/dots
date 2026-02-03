1af1c4721ba8c6d68b2de7a7a1beace282a25173
kr9sis <mkmw464l@anonaddy.com>
2026-02-03 08:45:35 +0000

    Updated waybar/charging.sh to remove race condition and only dismiss specific notification

--- waybar/.config/waybar/scripts/charging.sh
+++ waybar/.config/waybar/scripts/charging.sh
@@ -1,7 +1,14 @@
 #!/bin/env bash
 
-if grep -q -e "${1}" -e "${2}" <(makoctl list); then
-  makoctl dismiss
+mako_list=$(makoctl list)
+
+# Arguments 1 and 2 or defaults
+arg1="${1:-⚡ Time to recharge!}"
+arg2="${2:-⚡ RECHARGE NOW!}"
+
+if grep -q -e "$arg1" -e "${arg2}" <<<"$mako_list"; then
+  id=$(echo "$mako_list" | grep -oP 'Notification \K\d+')
+  makoctl dismiss "$id"
 fi
 
 notify-send '⚡ AC Connected!' 'Charging ' -u normal -t 2500
