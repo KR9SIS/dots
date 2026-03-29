7871176f9d5408a302e335ed8cc62290e560bba4
kr9sis <mkmw464l@anonaddy.com>
2026-03-29 00:58:40 +0000

    Optimized startup apps to not use magic constants for sleep

--- hyprland/.config/hypr/scripts/setup_home.sh
+++ hyprland/.config/hypr/scripts/setup_home.sh
@@ -1,28 +1,30 @@
-#! /bin/env bash
+#!/bin/env bash
 
 setup_app() {
   local workspace=$1
-  local class=$2
-  local title=$3
+  local monitor=$2
+  local class=$3
+  local title=$4
 
-  while ! (
-    hyprctl clients -j | jq -r --arg c "$class" --arg t "$title" '
-      .[]|select(.class==$c)|.title==$t '
-  ); do       # checking floating for vesktop
-    sleep 0.2 # simple and semi inefficient busy waiting loop
+  s_len=1
+
+  while ! window=$(hyprctl clients -j | jq -r --arg c "$class" --arg t "$title" \
+    '.[]|select(.class==$c and (.title | test($t; "i")))|.address' | head -1) || [[ -z "$window" ]]; do
+    sleep "$s_len"
+    s_len=$((s_len * 2))
   done
 
-  sleep 1
-  hyprctl dispatch movetoworkspacesilent "$workspace", "class:$class"
+  hyprctl dispatch movetoworkspacesilent "$workspace", "address:$window"
+
+  if hyprctl monitors | grep -q "(ID 1)"; then
+    hyprctl dispatch moveworkspacetomonitor "$workspace" "$monitor"
+  fi
 }
 
-setup_app 1 "brave-browser" "New tab - Brave"
-setup_app 2 "kitty" "~"
-setup_app 3 "vesktop" "Discord | Friends"
+setup_app 1 1 "brave-browser" "New tab - Brave"
+hyprctl dispatch workspace 2
+setup_app 2 0 "kitty" "~"
+hyprctl dispatch workspace 1
+setup_app 3 0 "vesktop" "Discord | Friends"
 
 hyprctl dispatch workspace 1
-
-if hyprctl monitors | grep -q "(ID 1)"; then
-  hyprctl dispatch moveworkspacetomonitor "2 0" # Move workspace 2 aka kitty to monitor 0 aka built in
-  hyprctl dispatch moveworkspacetomonitor "1 1" # Move workspace 1 aka brave-browser to monitor 1 aka second monitor
-fi
