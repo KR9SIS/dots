30fd901aedb51a672384ca9270a8d83249ea09fe
kr9sis <mkmw464l@anonaddy.com>
2026-04-16 23:45:57 +0000

    Updated charging script to have better variable names and use a temp file for charging status

--- waybar/.config/waybar/scripts/charging.sh
+++ waybar/.config/waybar/scripts/charging.sh
@@ -2,13 +2,17 @@
 
 mako_list=$(makoctl list)
 
-# Arguments 1 and 2 or defaults
-arg1="${1:-⚡ Time to recharge!}"
-arg2="${2:-⚡ RECHARGE NOW!}"
+# Arguments 1, 2 and 3 or defaults
+warn_notif="${1:-⚡ Time to recharge!}"
+crit_notif="${2:-⚡ RECHARGE NOW!}"
+tmp_file="/tmp/${3:-pcPluggedIn}"
 
-if grep -q -e "$arg1" -e "${arg2}" <<<"$mako_list"; then
+if grep -q -e "$warn_notif" -e "${crit_notif}" <<<"$mako_list"; then
   id=$(echo "$mako_list" | grep -oP 'Notification \K\d+')
   makoctl dismiss "$id"
 fi
 
-notify-send '⚡ AC Connected!' 'Charging ' -u normal -t 2500
+if [[ ! -f "$tmp_file" ]]; then
+  touch "$tmp_file"
+  notify-send '⚡ AC Connected!' 'Charging ' -u normal -t 2500
+fi
