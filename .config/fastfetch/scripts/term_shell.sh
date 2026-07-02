baf23c522424fb76ff9124f9a4de3019c245ef3b
kr9sis <mkmw464l@anonaddy.com>
2026-07-02 12:33:21 +0200

    Updated term_shell to show alacritty

--- .config/fastfetch/scripts/term_shell.sh
+++ .config/fastfetch/scripts/term_shell.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
+aVersion=$(alacritty --version | grep -Eo '^[a-z]+ [0-9]+\.[0-9]+\.[0-9]+')
 bVersion="bash $(echo "$BASH_VERSION" | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+')"
-kVersion=$(kitty --version | grep -Eo '^[a-z]+ [0-9]+\.[0-9]+\.[0-9]+')
 
-echo "$kVersion / $bVersion"
+echo "$aVersion / $bVersion"
