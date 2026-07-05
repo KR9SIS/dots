bfd7b6d75f75ab84901665832e3e40cf56bd1493
kr9sis <mkmw464l@anonaddy.com>
2026-07-05 23:40:04 +0200

    NixOS added default mimeApps

--- .config/nixos/home.nix
+++ .config/nixos/home.nix
@@ -93,6 +93,17 @@
         "gtk"
       ];
     };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/pdf" = "org.gnome.Evince.desktop";
+        "text/html" = "brave-browser.desktop";
+        "x-scheme-handler/http" = "brave-browser.desktop";
+        "x-scheme-handler/https" = "brave-browser.desktop";
+        "x-scheme-handler/about" = "brave-browser.desktop";
+        "x-scheme-handler/unknown" = "brave-browser.desktop";
+      };
+    };
     terminal-exec = {
       enable = true;
       settings = {
