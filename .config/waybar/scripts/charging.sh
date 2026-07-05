09fd7bf2bd3ed5968731132370f9f367a79ab401
kr9sis <mkmw464l@anonaddy.com>
2026-07-05 21:22:04 +0200

    Moved waybar to nixos and deleted unused scripts

--- .config/waybar/scripts/charging.sh
+++ .config/waybar/scripts/charging.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 mako_list=$(makoctl list)
 
--- .config/waybar/scripts/tray_icon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-if false; then # INFO: Make sure to always return false
-  echo '{"text": "", "tooltip": "Notification", "class": "active"}'
-else
-  echo '{"text": ""}'
-fi
