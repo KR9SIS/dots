9cbdbe88146869ba8ce33ed8ca70270bad87597c
kr9sis <mkmw464l@anonaddy.com>
2026-07-14 17:48:42 +0200

    Allowed launch-tmux to take cmd argument with send keys and setup default state for Alacritty and NixOS sessions

--- .config/hypr/autostart.lua
+++ .config/hypr/autostart.lua
@@ -9,6 +9,9 @@
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd("tmux new -s NixOS -d")
-	O.launch_or_focus({ focus = "launch-tmux", ws = 2, uwsm_args = "-T", args = TERM, class = TERM })
+	hl.timer(function()
+		hl.exec_cmd("tmux send-keys -t NixOS 'cd nixos && clear' Enter")
+	end, { type = "oneshot", timeout = 5000 })
+	O.launch_or_focus({ focus = "launch-tmux", ws = 2, uwsm_args = "-T", args = TERM .. " cf", class = TERM })
 	O.launch_or_focus({ focus = BROWSER, ws = 1, class = BROWSER })
 end)
